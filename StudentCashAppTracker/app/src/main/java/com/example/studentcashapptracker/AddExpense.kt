4caa8d7bc13bbb6e7c9ad31c999abe1985a92fdc
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2020-11-18 18:09:31 -0500

    Update AddExpense.kt

--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/AddExpense.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/AddExpense.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_expense.*
 
@@ -12,10 +13,21 @@ class AddExpense : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_expense)
+
 //        TODO: commit to a database
         val addExpense = findViewById<View>(R.id.addButton) as Button
         addExpense.setOnClickListener {
+            val category = editTextTextPersonName2.text
+            val cost = editTextNumberDecimal.text
+            val date = editTextDate2.text
+            val description = editTextTextMultiLine.text
 
+            editTextTextPersonName2.setText(" ")
+            editTextNumberDecimal.setText(" ")
+            editTextDate2.setText(" ")
+            editTextTextMultiLine.setText(" ")
+
+            Toast.makeText(getApplicationContext(), "Expense added!", Toast.LENGTH_SHORT).show();
         }
 
 //        TODO: go back to main screen
@@ -27,10 +39,11 @@ class AddExpense : AppCompatActivity() {
 //        TODO: clear the input boxes
         val clear = findViewById<View>(R.id.clearButton) as Button
         clear.setOnClickListener {
-            editTextDate2.setText(" ")
-            editTextNumberDecimal.setText(" ")
-            editTextTextMultiLine.setText(" ")
             editTextTextPersonName2.setText(" ")
+            editTextNumberDecimal.setText(" ")
+            editTextDate2.setText(" ")
+            editTextTextMultiLine.setText(" ")
+            Toast.makeText(getApplicationContext(), "Info cleared", Toast.LENGTH_SHORT).show();
         }
     }
 }
