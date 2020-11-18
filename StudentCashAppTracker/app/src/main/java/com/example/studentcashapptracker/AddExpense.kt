3c192a73ba8489638d2b6ffa87e1fed12b50a817
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2020-11-18 18:54:16 -0500

    Update AddExpense.kt

--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/AddExpense.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/AddExpense.kt
@@ -3,7 +3,9 @@ package com.example.studentcashapptracker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_expense.*
@@ -14,17 +16,41 @@ class AddExpense : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_expense)
 
+//        set up the category spinner
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+//      set up the 3 spinners for the date
+        val monthSpinner: Spinner = findViewById(R.id.spinner3)
+        ArrayAdapter.createFromResource(this, R.array.month, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            monthSpinner.adapter = adapter
+        }
+        val daySpinner: Spinner = findViewById(R.id.spinner4)
+        ArrayAdapter.createFromResource(this, R.array.day, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            daySpinner.adapter = adapter
+        }
+        val yearSpinner: Spinner = findViewById(R.id.spinner5)
+        ArrayAdapter.createFromResource(this, R.array.year, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            yearSpinner.adapter = adapter
+        }
+
 //        TODO: commit to a database
         val addExpense = findViewById<View>(R.id.addButton) as Button
         addExpense.setOnClickListener {
             val category = editTextTextPersonName2.text
             val cost = editTextNumberDecimal.text
-            val date = editTextDate2.text
+//            val date = editTextDate2.text
             val description = editTextTextMultiLine.text
 
+//             clear inputs after hitting add
             editTextTextPersonName2.setText(" ")
             editTextNumberDecimal.setText(" ")
-            editTextDate2.setText(" ")
+//            editTextDate2.setText(" ")
             editTextTextMultiLine.setText(" ")
 
             Toast.makeText(getApplicationContext(), "Expense added!", Toast.LENGTH_SHORT).show();
@@ -41,7 +67,7 @@ class AddExpense : AppCompatActivity() {
         clear.setOnClickListener {
             editTextTextPersonName2.setText(" ")
             editTextNumberDecimal.setText(" ")
-            editTextDate2.setText(" ")
+//            editTextDate2.setText(" ")
             editTextTextMultiLine.setText(" ")
             Toast.makeText(getApplicationContext(), "Info cleared", Toast.LENGTH_SHORT).show();
         }
