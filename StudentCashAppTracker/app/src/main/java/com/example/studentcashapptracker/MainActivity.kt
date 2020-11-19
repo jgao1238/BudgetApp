d8d2dec3c9d7dc65d5bf330047095aa707c25f0e
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2020-11-19 14:24:08 -0500

    Update MainActivity.kt

--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/MainActivity.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.studentcashapptracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -17,42 +18,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val editButton = findViewById<View>(R.id.editButton) as Button
         editButton.setOnClickListener {
-
-            build = AlertDialog.Builder(this).setTitle("Edit")
-            val dialogBox = build.create()
-            dialogBox.show()
+//            val edit = Intent(this@MainActivity, EditInfo::class.java)
+//            startActivityForResult(edit,0)
         }
-
 
         val addExpenseButton = findViewById<View>(R.id.addExpenseButton) as Button
         addExpenseButton.setOnClickListener {
-            build = AlertDialog.Builder(this).setTitle("Add Expense")
-            val customLayout: View = layoutInflater.inflate(R.layout.add_expense, null)
-
-//            add button
-            build.setPositiveButton("Add") { dialog, which ->
-                val category = customLayout.findViewById<EditText>(R.id.category)
-                Toast.makeText(this, category.text, Toast.LENGTH_SHORT).show()
-                Thread.sleep(1000L)
-                val cost = customLayout.findViewById<EditText>(R.id.cost)
-                Toast.makeText(this, cost.text, Toast.LENGTH_SHORT).show()
-                Thread.sleep(1000L)
-                val date = customLayout.findViewById<EditText>(R.id.date)
-                Toast.makeText(this, date.text, Toast.LENGTH_SHORT).show()
-                Thread.sleep(1000L)
-                val description = customLayout.findViewById<EditText>(R.id.description)
-                Toast.makeText(this, description.text, Toast.LENGTH_SHORT).show()
-            }
-//            cancel button
-            build.setNegativeButton("Cancel") { dialog, which ->
-            }
-
-
-            build.setView(customLayout)
-            val dialogBox = build.create()
-            dialogBox.show()
+            val addExpense = Intent(this@MainActivity, AddExpense::class.java)
+            startActivityForResult(addExpense,0)
         }
 
     }
@@ -64,6 +41,8 @@ class MainActivity : AppCompatActivity() {
     }
 //    menu dialog
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val previousActivity = Intent(this@MainActivity, PreviousPeriods::class.java)
+//        startActivityForResult(previousActivity,0 )
         build = AlertDialog.Builder(this).setMessage("Display Previous Months as clickable")
         val dialogBox = build.create()
         dialogBox.show()
