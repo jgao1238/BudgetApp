0352c1bf1c81f7771e7289ab4886a54b32f6b390
Matthew Sandler <sandlerm@umd.edu>
2020-11-25 14:10:42 -0500

    Update MainActivity.kt

--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/MainActivity.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -26,11 +27,42 @@ class MainActivity : AppCompatActivity() {
 // //            startActivityForResult(edit,0)
 //         }
 
-        val addExpenseButton = findViewById<View>(R.id.addExpenseButton) as Button
+        val addExpenseButton = findViewById<Button>(R.id.addExpenseButton)
         addExpenseButton.setOnClickListener {
             val addExpense = Intent(this@MainActivity, AddExpense::class.java)
             startActivityForResult(addExpense,0)
         }
+
+        val foodButton = findViewById<Button>(R.id.foodButton)
+        foodButton.setOnClickListener {
+            val toFood = Intent(this@MainActivity, Food::class.java)
+            startActivity(toFood)
+        }
+
+        val carButton = findViewById<Button>(R.id.carButton)
+        carButton.setOnClickListener {
+            val toCar = Intent(this@MainActivity, Car::class.java)
+            startActivity(toCar)
+        }
+
+        val healthButton = findViewById<Button>(R.id.healthButton)
+        healthButton.setOnClickListener {
+            val toHealth = Intent(this@MainActivity, Health::class.java)
+            startActivity(toHealth)
+        }
+
+        val rentButton = findViewById<Button>(R.id.rentButton)
+        rentButton.setOnClickListener {
+            val toRent = Intent(this@MainActivity, Rent::class.java)
+            startActivity(toRent)
+        }
+
+        val otherButton = findViewById<Button>(R.id.foodButton)
+        otherButton.setOnClickListener {
+            val toOther = Intent(this@MainActivity, Other::class.java)
+            startActivity(toOther)
+        }
+
 
     }
     //    crate the menu in the top right of the screen
