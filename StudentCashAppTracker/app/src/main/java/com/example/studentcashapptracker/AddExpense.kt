75abae15208cc46ee7451d1225554c232a55a70a
jgao1238 <46405116+jgao1238@users.noreply.github.com>
2020-11-28 11:59:43 -0500

    Initial commit

--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/AddExpense.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/AddExpense.kt
@@ -1,7 +1,9 @@
 package com.example.studentcashapptracker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -9,20 +11,32 @@ import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_expense.*
+import org.json.JSONObject
+import java.io.BufferedWriter
+import java.io.FileNotFoundException
+import java.io.FileWriter
+import java.io.OutputStreamWriter
 
 class AddExpense : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "AddExpense"
+        private const val FILE_NAME = "TestFile.txt"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_expense)
 
-//        set up the category spinner
+        val trackPeriod = intent.getIntExtra("trackPeriod",0) //Period number
+
+        //set up the category spinner
         val spinner: Spinner = findViewById(R.id.spinner)
         ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
         }
-//      set up the 3 spinners for the date
+        //set up the 3 spinners for the date
         val monthSpinner: Spinner = findViewById(R.id.spinner3)
         ArrayAdapter.createFromResource(this, R.array.month, android.R.layout.simple_spinner_item).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -39,17 +53,60 @@ class AddExpense : AppCompatActivity() {
             yearSpinner.adapter = adapter
         }
 
-//        TODO: commit to a database
+        //User is able to enter in as many entries as they want without going back to the screen
         val addExpense = findViewById<View>(R.id.addButton) as Button
         addExpense.setOnClickListener {
             val category = spinner.selectedItem
             val cost = editTextNumberDecimal.text
-            val month = spinner3.selectedItem
+            var month = spinner3.selectedItem
             val day = spinner4.selectedItem
             val year = spinner5.selectedItem
             val description = editTextTextMultiLine.text
 
-//             clear inputs after hitting add
+            when(month){ //Changing the month to a date for displaying later
+                "January" -> month = "1"
+                "February" -> month = "2"
+                "March" -> month = "3"
+                "April" -> month = "4"
+                "May" -> month = "5"
+                "June" -> month = "6"
+                "July" -> month = "7"
+                "August" -> month = "8"
+                "September" -> month = "9"
+                "October" -> month = "10"
+                "November" -> month = "11"
+                "December" -> month = "12"
+            }
+            val date = (month as String) + "/" + (day as String) + "/" + (year as String)
+
+            val jsonObject = JSONObject()
+            jsonObject.put("category",category)
+            jsonObject.put("cost",cost)
+            jsonObject.put("date",date)
+            jsonObject.put("description",description)
+            jsonObject.put("period",trackPeriod)
+            var entry = jsonObject.toString() //Convert JSON to string
+
+            if(!getFileStreamPath(FILE_NAME).exists()){
+                try {
+                    val fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE)
+                    val writer = BufferedWriter(OutputStreamWriter(fos))
+                    writer.write(entry)
+                    writer.write("\n")
+                    writer.close()
+                } catch (e: FileNotFoundException) {
+                    Log.i(TAG, "FileNotFoundException")
+                }
+            } else {
+                //File already exists
+                val fos = openFileOutput(FILE_NAME,Context.MODE_APPEND)
+                val writer = BufferedWriter(OutputStreamWriter(fos))
+                writer.write(entry)
+                writer.write("\n")
+                writer.close()
+            }
+
+            //Resets fields and creates Toast notifying success
             spinner.setSelection(0, true)
             editTextTextPersonName2.setText(" ")
             editTextNumberDecimal.setText(" ")
@@ -57,16 +114,16 @@ class AddExpense : AppCompatActivity() {
             spinner4.setSelection(0, true)
             spinner5.setSelection(0, true)
             editTextTextMultiLine.setText(" ")
-
-            Toast.makeText(getApplicationContext(), "Expense added!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Expense added!", Toast.LENGTH_SHORT).show()
         }
 
-//        TODO: go back to main screen
+        //Cancel leads back to the main screen
         val cancel = findViewById<View>(R.id.cancelButton) as Button
         cancel.setOnClickListener {
-           finish()
+            finish()
         }
-//        TODO: clear the input boxes
+
+        //Clear resets the form to the default values
         val clear = findViewById<View>(R.id.clearButton) as Button
         clear.setOnClickListener {
             spinner.setSelection(0, true)
--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/MainActivity.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/MainActivity.kt
@@ -1,77 +1,152 @@
 package com.example.studentcashapptracker
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.SeekBar
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.*
+import java.lang.Exception
+import kotlin.properties.Delegates
 
+//Overall task list
+//TODO: editing and deleting entries for time period
+//TODO: create ExpandableListView/display each entry for current time period
+//TODO: display history of periods
+//TODO: end tracking period
+
+//Do now
+//
 
 class MainActivity : AppCompatActivity() {
     private lateinit var build: AlertDialog.Builder
+
+    private var budgetValue: Int = 0
+    private var budgetPos: Int = 0
+    private var trackPeriod:Int = 0
+
+    protected lateinit var sharedpreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sharedpreferences = getSharedPreferences(mypreference,Context.MODE_PRIVATE)
 
-//         val editButton = findViewById<View>(R.id.editButton) as Button
-//         editButton.setOnClickListener {
-// //            val edit = Intent(this@MainActivity, EditInfo::class.java)
-// //            startActivityForResult(edit,0)
-//         }
+        trackPeriod = sharedpreferences.getInt("trackPeriod",0)
+        budgetValue = sharedpreferences.getInt("budgetValue",0)
+        budgetPos = sharedpreferences.getInt("budgetPos",0)
 
-        val addExpenseButton = findViewById<Button>(R.id.addExpenseButton)
+        val maxBudget = findViewById<View>(R.id.maxLimitAmountSlider) as SeekBar
+        val maxField = findViewById<View>(R.id.maxLimitAmountText) as EditText
+        maxField.setText(budgetValue.toString())
+        maxBudget.setProgress(budgetPos.toInt())
+        //Change the seekBar if EditText changes
+        maxField.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                try{
+                    budgetValue = p0.toString().toInt()
+                    maxBudget.setProgress(budgetValue)
+                    budgetPos = maxBudget.progress
+                    val editor = sharedpreferences.edit()
+                    editor.putInt("budgetValue",budgetValue)
+                    editor.putInt("budgetPos",budgetPos)
+                    editor.apply()
+                } catch(e: Exception) {}
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+        //Change the EditText if the seekBar changes
+        maxBudget.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                budgetPos = progress
+                maxField.setText(progress.toString())
+                budgetValue = maxField.text.toString().toInt()
+                val editor = sharedpreferences.edit()
+                editor.putInt("budgetValue",budgetValue)
+                editor.putInt("budgetPos",budgetPos)
+                editor.apply()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+        //Launches screen to add expenses
+        val addExpenseButton = findViewById<View>(R.id.addExpenseButton) as Button
         addExpenseButton.setOnClickListener {
             val addExpense = Intent(this@MainActivity, AddExpense::class.java)
+            addExpense.putExtra("trackPeriod",trackPeriod)
             startActivityForResult(addExpense,0)
         }
 
-        val foodButton = findViewById<Button>(R.id.foodButton)
-        foodButton.setOnClickListener {
-            val toFood = Intent(this@MainActivity, Food::class.java)
-            startActivity(toFood)
+        val endPeriod = findViewById<View>(R.id.endTrackingPeriodButton) as Button
+        endPeriod.setOnClickListener{
+            trackPeriod += 1
+            val editor = sharedpreferences.edit()
+            editor.putInt("trackPeriod",trackPeriod)
+            editor.apply()
         }
-
-        val carButton = findViewById<Button>(R.id.carButton)
-        carButton.setOnClickListener {
-            val toCar = Intent(this@MainActivity, Car::class.java)
-            startActivity(toCar)
-        }
-
-        val healthButton = findViewById<Button>(R.id.healthButton)
-        healthButton.setOnClickListener {
-            val toHealth = Intent(this@MainActivity, Health::class.java)
-            startActivity(toHealth)
-        }
-
-        val rentButton = findViewById<Button>(R.id.rentButton)
-        rentButton.setOnClickListener {
-            val toRent = Intent(this@MainActivity, Rent::class.java)
-            startActivity(toRent)
-        }
-
-        val otherButton = findViewById<Button>(R.id.foodButton)
-        otherButton.setOnClickListener {
-            val toOther = Intent(this@MainActivity, Other::class.java)
-            startActivity(toOther)
-        }
-
 
     }
-    //    crate the menu in the top right of the screen
+
+    override fun onStart() {
+        //Initializing the total summary at the top + update when we come back from add screen
+        var amtSpent = findViewById<TextView>(R.id.amountSpentValue)
+        try {
+            val reader = BufferedReader(InputStreamReader(openFileInput(FILE_NAME)))
+            var line = reader.readLine()
+            var testing = JSONArray()
+            while(line != null){
+                val sb = StringBuilder()
+                sb.append(line)  //Might need to use sb.append(line).append("\n") if error
+                val jsonObject = JSONObject(sb.toString())
+                testing.put(jsonObject)
+                line = reader.readLine()
+            }
+            reader.close()
+            var mVal = 0
+            for(i in 0 until testing.length()){
+                val entry = testing.getJSONObject(i)
+                //There's going to be a lot of entries, so only add the current period's
+                if(entry.get("period").toString().toInt() == trackPeriod){
+                    mVal += entry.get("cost").toString().toInt()
+                }
+            }
+            amtSpent.text = mVal.toString()
+        } catch(e: IOException){
+            Log.i(TAG,"IOException")
+        }
+        super.onStart()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    //create the menu in the top right of the screen
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_file, menu)
         return true
     }
-//    menu dialog
+
+    //menu dialog
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 //        val previousActivity = Intent(this@MainActivity, PreviousPeriods::class.java)
 //        startActivityForResult(previousActivity,0 )
@@ -79,5 +154,11 @@ class MainActivity : AppCompatActivity() {
         val dialogBox = build.create()
         dialogBox.show()
         return true
+    }
+
+    companion object {
+        private const val mypreference = "mypref"
+        private const val TAG = "MainActivity"
+        private const val FILE_NAME = "TestFile.txt"
     }
 }
