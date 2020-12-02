fb395424bd5e379b4826efa9992b93c86b1f0afc
jgao1238 <46405116+jgao1238@users.noreply.github.com>
2020-12-02 16:06:23 -0500

    aesthetic changes

--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/Car.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/Car.kt
@@ -1,9 +1,14 @@
 package com.example.studentcashapptracker
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.util.Log
 import android.widget.ListView
 import android.widget.SimpleAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONArray
 import org.json.JSONObject
@@ -13,11 +18,14 @@ import java.io.InputStreamReader
 
 
 class Car: AppCompatActivity(){
+    protected lateinit var sharedpreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.car_layout)
 
         var entries = JSONArray()
+        var trackPeriod: Int = 0
         val list: MutableList<HashMap<String,String>> = ArrayList()
         try {
             val reader = BufferedReader(InputStreamReader(openFileInput("TestFile.txt")))
@@ -30,17 +38,20 @@ class Car: AppCompatActivity(){
             }
             reader.close()
             for(i in 0 until entries.length()){
+                sharedpreferences = getSharedPreferences("mypref", Context.MODE_PRIVATE)
+                trackPeriod = sharedpreferences.getInt("trackPeriod",0)
                 val entry = entries.getJSONObject(i)
-                var item = HashMap<String, String>()
-                item.put("line1", "Date: " + entry.get("date").toString())
-                item.put("line2", "Cost: $" + entry.get("cost").toString())
-                item.put("line3", "Description: " + entry.get("description").toString())
-                list.add(item)
+                if(entry.get("period").toString().toInt() == trackPeriod && entry.get("category").toString() == "Car"){
+                    var item = HashMap<String, String>()
+                    item.put("line1", "Date: " + entry.get("date").toString())
+                    item.put("line2", "Cost: $" + entry.get("cost").toString())
+                    item.put("line3", "Description: " + entry.get("description").toString())
+                    list.add(item)
+                }
             }
             val adapt = SimpleAdapter(this,list, R.layout.display_entries, arrayOf("line1","line2","line3"),
                 intArrayOf(R.id.line_a,R.id.line_b,R.id.line_c)
             )
-
             val testing = findViewById<ListView>(R.id.carList)
             testing.adapter = adapt
         } catch(e: IOException){
--- StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/Rent.kt
+++ StudentCashAppTracker/app/src/main/java/com/example/studentcashapptracker/Rent.kt
@@ -1,11 +1,57 @@
 package com.example.studentcashapptracker
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class Rent: AppCompatActivity(){
+    protected lateinit var sharedpreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.rent_layout)
+
+        var entries = JSONArray()
+        var trackPeriod: Int = 0
+        val list: MutableList<HashMap<String,String>> = ArrayList()
+        try {
+            val reader = BufferedReader(InputStreamReader(openFileInput("TestFile.txt")))
+            var line = reader.readLine()
+            while(line != null){
+                val sb = StringBuilder()
+                sb.append(line)
+                entries.put(JSONObject(sb.toString()))
+                line = reader.readLine()
+            }
+            reader.close()
+            for(i in 0 until entries.length()){
+                sharedpreferences = getSharedPreferences("mypref", Context.MODE_PRIVATE)
+                trackPeriod = sharedpreferences.getInt("trackPeriod",0)
+                val entry = entries.getJSONObject(i)
+                if(entry.get("period").toString().toInt() == trackPeriod && entry.get("category").toString() == "Rent"){
+                    var item = HashMap<String, String>()
+                    item.put("line1", "Date: " + entry.get("date").toString())
+                    item.put("line2", "Cost: $" + entry.get("cost").toString())
+                    item.put("line3", "Description: " + entry.get("description").toString())
+                    list.add(item)
+                }
+            }
+            val adapt = SimpleAdapter(this,list, R.layout.display_entries, arrayOf("line1","line2","line3"),
+                intArrayOf(R.id.line_a,R.id.line_b,R.id.line_c)
+            )
+            val testing = findViewById<ListView>(R.id.rentList)
+            testing.adapter = adapt
+        } catch(e: IOException){
+            Log.i("Car.kt","IOException")
+        }
     }
 }
